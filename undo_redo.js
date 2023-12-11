29526448f941979eff53ecdbaa7129ef91756fe1
Pierism <mnurasha2000@gmail.com>
2023-12-12 01:03:56 +0530

    add undo and redo feature

---
+++ undo_redo.js
@@ -0,0 +1,17 @@
+function undo(value=null) {
+                if(value){
+                    document.execCommand("undo", false, value);
+                } else {
+                    document.execCommand("undo");
+             }
+        }
+
+
+function redo(value=null) {
+                if(value){
+                    document.execCommand("redo", false, value);
+                } 
+                else {
+                    document.execCommand("redo");
+                 }
+        }
